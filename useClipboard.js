6c0afb307b25e73882e23cf780bde50d76917743
Naseem Ansari <118877331+Gitnaseem745@users.noreply.github.com>
2024-12-06 21:37:06 +0530

    #utility to copy text to the clipboard

---
+++ useClipboard.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const useClipboard = () => {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    });
+  };
+
+  return { copied, copyToClipboard };
+};
+
+export default useClipboard;
