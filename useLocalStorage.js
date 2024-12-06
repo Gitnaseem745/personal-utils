5795af1d22caa4e21e50143c4449fcdb55b554cd
Naseem Ansari <118877331+Gitnaseem745@users.noreply.github.com>
2024-12-06 21:40:58 +0530

    #utility to Saving and retrieving data from localStorage.

---
+++ useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : initialValue;
+  });
+
+  const setValue = (value) => {
+    setStoredValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
